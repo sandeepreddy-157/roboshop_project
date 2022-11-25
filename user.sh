9a4b1cf8915cbb53c7eaf5a7ded5a5408fe1104f
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2022-11-25 13:36:37 +0530

    user service

--- user.sh
+++ user.sh
@@ -21,8 +21,8 @@ useradd roboshop
 
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
 cd /home/roboshop
-unzip -o /tmp/user.zip
 rm -rf user
+unzip -o /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
