6edd75029f98df3ff5855387007da0b6d2c6f7af
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2022-11-27 19:10:53 +0530

    mysql service

--- mysql.sh
+++ mysql.sh
@@ -23,5 +23,5 @@ systemctl enable mysqld
 systemctl start mysqld
 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/root-pass-sql
-DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+DEFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
