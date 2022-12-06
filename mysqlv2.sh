e2a0d2e8ca6f565f6dddf7df2a14e4e411649398
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2022-12-06 19:25:46 +0530

    MYSQL service version2 for roboshop project

--- mysqlv2.sh
+++ mysqlv2.sh
@@ -1,64 +1,58 @@
-MySQL is the database service which is needed for the application. So we need to install it and configure it for the application to work.
-## **Manual Steps to Install MySQL**
-
-#As per the Application need, we are choosing MySQL 5.7 version.
-
-#1. Setup Repo
-#On Centos-7
-
-# curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-
-#On Centos-8
+COMPONENT=mysql
+source common.sh
 
 if [ -z '$1']; then
-  echo input argument password is needed
+  echo "input argument password is needed"
   exit 1
 fi
 ROBOSHOP_MYSQL_PASSWORD=$1
 
-STAT() {
-  if [ $1 -eq 0 ]
-  then
-    echo -e "\e[32m SUCCESS\e[0m"
-  else
-    echo -e "\e[31m FAILURE\e[0m"
-    exit 1
-  fi
-}
 
-PRINT() {
-  echo -e "\e[33m$1\e[0m"
-}
-PRINT -e "\e[31m DOWNLOADING MYSQL REPO FILE\e[0m"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+PRINT "DOWNLOADING MYSQL REPO FILE"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo  &>>$LOG
 STAT $?
 
 PRINT "DISABLE MODULE FOR MYSQL 8 VERSION REPO"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>$LOG
 STAT $?
 
 #Install MySQL
 
 PRINT "INSTALLING MYSQL SERVER"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>$LOG
 STAT $?
 
 #Start MySQL
 PRINT "ENABLING MYSQL SERVICE"
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOG
 STAT $?
 
 PRINT "START MYSQL SERVICE"
 
-systemctl start mysqld
+systemctl start mysqld &>>$LOG
 STAT $?
 
-echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
+echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}  &>>$LOG
 if [ $? -ne 0 ]
 then
   echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
   DEFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" &>>$LOG
 fi
 
-#while executing we need to pri
+PRINT "Uninstall Plugin Validate Password"
+echo "show plugins" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>$LOG
+if [ $? -eq 0 ]; then
+  echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG
+fi
+STAT $?
+
+APP_LOC=/tmp
+CONTENT=mysql-main
+DOWNLOAD_APP_CODE
+
+cd ${CONTENT} &>>$LOG
+
+PRINT "LOAD SHIPPING SCHEMA"
+mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG
+STAT $?
