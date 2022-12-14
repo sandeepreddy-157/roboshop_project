ba4aabada1ce5447cc56fdc047b22d755502ea32
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2022-12-14 21:07:39 +0530

    frontend script file is for frontend component in roboshop project

--- frontendv2.sh
+++ frontendv2.sh
@@ -16,6 +16,10 @@ PRINT "Copy RoboShop Configuration File"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT $?
 
+PRINT "Updating roboshop configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.happylearning.buzz/' -e '/user/ s/localhost/dev-user.happylearning.buzz/' -e '/cart/ s/localhost/dev-cart.happylearning.buzz/' -e '/shipping/ s/localhost/dev-shipping.happylearning.buzz/' -e '/payment/ s/localhost/dev-payment.happylearning.buzz/'  /etc/nginx/default.d/roboshop.conf &>>LOG
+STAT $?
+
 PRINT "ENABLE NGINX SERVICE"
 sudo systemctl enable nginx &>>$LOG
 STAT $?
